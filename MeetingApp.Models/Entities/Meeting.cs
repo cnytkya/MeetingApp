d9f33b326ca381fd6df5ad80113c8a925495dbe8
Cüneyt Kaya <kayacuneyt2542@gmail.com>
2023-11-23 15:59:32 +0300

    Modelleri ve propertylerini tanımladık.

---
+++ MeetingApp.Models/Entities/Meeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingApp.Models.Entities
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Description { get; set; }
+
+        public string DocumentPath { get; set; }
+    }
+
+}
