d9180f34757dbb64e8dee0b7f49d87e28102adfc
Cüneyt Kaya <kayacuneyt2542@gmail.com>
2023-11-23 16:36:16 +0300

    AppDbContext sınıfımızı oluştururak veri tabanı configurationlarını hazırladık. ConnectionString ile veritabanı bağlantısını gerçekleştirmiş olduk.

---
+++ MeetingApp.DataLayer/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MeetingApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingApp.DataLayer
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+    }
+}
--- MeetingApp.MVC/Program.cs
+++ MeetingApp.MVC/Program.cs
@@ -1,7 +1,12 @@
+using MeetingApp.DataLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseSqlServer(builder.Configuration.GetConnectionString("")));
 
 var app = builder.Build();
 
