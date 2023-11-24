5e3546e257389b37e68dcc852c77ee28588d8c6e
Cüneyt Kaya <kayacuneyt2542@gmail.com>
2023-11-24 13:01:45 +0300

    Service ve Manager sınıflarımızı oluşturduk. Burda bir sınıfın sadece bir sorumluluğu olmalı prensibini uyguladık. SOLID PRENCIPLE => SRP presibi.

---
+++ MeetingApp.BusinessLayer/Manager/MeetingManager.cs
@@ -0,0 +1,47 @@
+﻿using MeetingApp.BusinessLayer.Service;
+using MeetingApp.DataLayer.Interface;
+using MeetingApp.Models.Entities;
+
+namespace MeetingApp.BusinessLayer.Manager
+{
+    public class MeetingManager : IMeetingService
+    {
+        IMeetingRepository _repo;
+
+        public MeetingManager(IMeetingRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public Task<IEnumerable<Meeting>> GetAllAsync()
+        {
+            return _repo.GetAllAsync();
+        }
+
+        public Task<Meeting> TGetByIdAsync(int id)
+        {
+            return _repo.GetByIdAsync(id);
+        }
+
+        public Task TAddAsync(Meeting entity)
+        {
+            return _repo.AddAsync(entity);
+        }
+
+        public Task TUpdateAsync(Meeting entity)
+        {
+            return _repo.UpdateAsync(entity);
+        }
+
+        public Task TDeleteAsync(int id)
+        {
+            return _repo.DeleteAsync(id);
+        }
+
+        public Task<IEnumerable<Meeting>> GetMeetingsByDateRangeAsync(DateTime startDate, DateTime endDate)
+        {
+            return _repo.GetMeetingsByDateRangeAsync(startDate, endDate);
+        }
+    }
+
+}
---
+++ MeetingApp.BusinessLayer/Service/IMeetingService.cs
@@ -0,0 +1,15 @@
+﻿using MeetingApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingApp.BusinessLayer.Service
+{
+    public interface IMeetingService : IGenericService<Meeting>
+    {
+        Task<IEnumerable<Meeting>> GetMeetingsByDateRangeAsync(DateTime startDate, DateTime endDate);
+    }
+
+}
