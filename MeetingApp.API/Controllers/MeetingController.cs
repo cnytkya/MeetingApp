b2ce9ec5bb44568286027c1edba72eb6aa85cc68
Cüneyt Kaya <kayacuneyt2542@gmail.com>
2023-12-02 22:06:23 +0300

    WebApi katmanı CRUD işlemleri

---
+++ MeetingApp.API/Controllers/MeetingController.cs
@@ -0,0 +1,85 @@
+﻿using MeetingApp.DataLayer.Interface;
+using MeetingApp.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeetingController : ControllerBase
+    {
+        private readonly IMeetingRepository _mRepo;
+
+        public MeetingController(IMeetingRepository mRepo)
+        {
+            _mRepo = mRepo;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Meeting>> GetMeetings()
+        {
+            var meetings = _mRepo.GetAll().ToList();
+            return Ok(meetings);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Meeting> GetMeetingById(int id)
+        {
+            var meeting = _mRepo.Get(x => x.Id == id);
+
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(meeting);
+        }
+
+        [HttpPost]
+        public ActionResult<Meeting> CreateMeeting(Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                _mRepo.Add(meeting);
+                _mRepo.Save();
+                return CreatedAtAction(nameof(GetMeetingById), new { id = meeting.Id }, meeting);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateMeeting(int id, Meeting meeting)
+        {
+            if (id != meeting.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mRepo.Update(meeting);
+                _mRepo.Save();
+                return NoContent();
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMeeting(int id)
+        {
+            var meeting = _mRepo.Get(x => x.Id == id);
+
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            _mRepo.Remove(meeting);
+            _mRepo.Save();
+
+            return NoContent();
+        }
+    }
+}
--- MeetingApp.API/Program.cs
+++ MeetingApp.API/Program.cs
@@ -1,3 +1,8 @@
+using MeetingApp.DataLayer;
+using MeetingApp.DataLayer.Interface;
+using MeetingApp.DataLayer.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IMeetingRepository, MeetingRepository>();
 
 var app = builder.Build();
 
